4517de019d52fbfa168c9a7e529fc37db4e6460d
SajidMajeed92 <sajidmajeed.ist@gmail.com>
2023-08-07 15:51:07 +0500

    update project

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+
+
+
+
 
 function App() {
+  const[todos,setTodos]=useState(["Todo App with Firebase"])
+  const [text, setText] = useState("")
+
+// Add Element in a List
+  const Add=()=>{
+
+  }
+  // Update Element in a List
+  const update=()=>{
+
+  }
+  
+  // Delete Element in a List
+
+  const Delete=()=>{
+// Delete All Element in a List
+  }
+  const DeleteAll=()=>{
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World.....!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className='Todo_Header'>☑ TODO APP</h1>
+    <form>
+      <Paper elevation={5} className="Add_Todo">
+        <TextField className = "Text_Field" label = "Enter Todos" value ={text} onChange={(e)=>setText(e.target.value)}><br/></TextField>
+        <Button type = "submit" variant='contained' color='Primary' onClick={(Add)}>Add</Button>
+        <Button  variant='contained' color='Primary' onClick={(DeleteAll)}>DeleteAll</Button>
+
+      </Paper>
+    </form>
+    {todos.map((data,i)=>{
+  return<p>{i+1}.{data} </p>
+    })
+    
+    }
+    
     </div>
   );
 }
